9f9c9a3b2bb75fd25aefa880ea42857dc6b12516
camorezka <sadovoyura777333arbuz@gmail.com>
2026-09-23 20:38:00 +0300

    Return profile login timestamp from card bootstrap

--- api/bot.js
+++ api/bot.js
@@ -629,7 +629,7 @@ export default async function handler(req, res) {
         })
       });
       const cards = await sb(`collectibles?owner_id=eq.${user.id}&select=id,skin_id,card_username,title,description,visibility,transfer_count,acquisition_type,created_at&order=created_at.asc`);
-      return res.status(200).json({ ok: true, cards: cards || [], card: cards?.[0] || null });
+      return res.status(200).json({ ok: true, cards: cards || [], card: cards?.[0] || null, last_login_at: loginAt });
     }
 
     // ---- 3c) CHECK 4-DIGIT CARD NUMBER -----------------------------
